f62b18f32fa32e2084d056f78b1a4703a7c6c955
Devin <devinazzie@gmail.com>
2020-02-27 07:22:19 +0200

    Tried asynchronous function to pull data from GH

--- Test.js
+++ Test.js
@@ -1,27 +1,89 @@
-function getGradInfo(){
+// function getGradInfo(){
 
-    //let requestURL = 'https://raw.githubusercontent.com/Gradgital/Doctrina2020/master/database/data.json';
-    //let request = new XMLHttpRequest();
-    //request.open('GET', requestURL);
+    // let requestURL = 'https://raw.githubusercontent.com/Gradgital/Doctrina2020/master/database/data.json';
+    // let request = new XMLHttpRequest();
+    // request.open('GET', requestURL);
 
-    //request.responseType = 'json';
-    //request.send();
-    //request.onload = function() {const graduates = request.response;}
-
-    var json = require('./data.json'); //(with path)
-    var grads = JSON.parse(json);
+    // request.responseType = 'json';
+    // request.send();
+    // var  graduates = NaN;
     
-    // var Grads = [];
-    // for (var i=0; i<json.length; i++) {
-    //     Grads[i] = {
-    //         photo: graduates[i]["Photo"],
-    //         name: graduates[i]["Name"],
-    //         year: graduates[i]["Year"],
-    //         program: graduates[i]["Program"],
-    //         githublink: graduates[i]["GithubLink"],
-    //     };
+    // request.onload = function() {
+    //    let graduates = request.response;
     // }
-    // console.log(Grads)
+    // console.log(graduates)
+
+
+
+//     var json = require('C:\Users\devin.azzie\Documents\GitHub\JavascriptStuff\data.json'); //(with path)
+//     var grads = JSON.parse(json);
     
-    return grads;
+//     // var Grads = [];
+//     // for (var i=0; i<json.length; i++) {
+//     //     Grads[i] = {
+//     //         photo: graduates[i]["Photo"],
+//     //         name: graduates[i]["Name"],
+//     //         year: graduates[i]["Year"],
+//     //         program: graduates[i]["Program"],
+//     //         githublink: graduates[i]["GithubLink"],
+//     //     };
+//     // }
+//     // console.log(Grads)
+
+    
+
+//      return grads;
+//  }
+// var grads = JSON.parse(json);
+
+class FileReader{
+    constructor(fileLoc){
+        this.Data = null;
+        this.set = false;
+        this.readTextFile(fileLoc, this.CBReader);
+    }
+
+    readTextFile(file, callback) {
+        var rawFile = new XMLHttpRequest();
+        rawFile.overrideMimeType("application/json");
+        rawFile.open("GET", file, true);
+        rawFile.onreadystatechange = function() {
+            if (rawFile.readyState === 4 && rawFile.status == "200") {
+                var data = callback(rawFile.responseText)
+                this.set = true;
+                this.Data = data;
+                
+            }
+        }
+        rawFile.send(null);
+    }
+    
+    CBReader(text){
+        console.log(text);
+        var Grads = JSON.parse(text);
+        return Grads;
+    }
+
+    sendData(){
+        return this.Data;
+    }
+
 }
+
+var source = new FileReader('https://raw.githubusercontent.com/Gradgital/Doctrina2020/master/database/data.json');
+console.log("Run function");
+console.log(source.sendData());
+
+
+
+//usage:
+
+
+// fetch('https://raw.githubusercontent.com/Gradgital/Doctrina2020/master/database/data.json')
+//   .then(res => res.json())
+//   .then(json => {
+//     //json vaiable contains object with data
+//   })
+
+//   console.log(json)
+
